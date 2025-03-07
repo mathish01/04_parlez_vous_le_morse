6e16441e98aeed05985dbc1372c550fee1091e2f
Mathis Holler <m.holler@orange.fr>
2025-03-07 13:17:59 +0100

    Version 2

---
+++ script.js
@@ -0,0 +1,77 @@
+
+
+function getLatinCharacterList (text) {
+    return text.split('')
+
+} 
+
+const result = getLatinCharacterList("Hello World")
+console.log (result)
+
+
+
+
+
+function translateLatinCharacter (character) {
+
+    const latinToMorse = {
+        'A':'.-',
+        'B':'-...',
+        'C':'-.-.',
+        'D':'-..',
+        'E':'.',
+        'F':'..-.',
+        'G':'--.',
+        'H':'....',
+        'I':'..',
+        'J':'.---',
+        'K':'-.-',
+        'L':'.-..',
+        'M':'--',
+        'N':'-.',
+        'O':'---',
+        'P':'.--.',
+        'Q':'--.-',
+        'R':'.-.',
+        'S':'...',
+        'T':'-',
+        'U':'..-',
+        'V':'...-',
+        'W':'.--',
+        'X':'-..-',
+        'Y':'-.--',
+        'Z':'--..'
+    }
+if (latinToMorse[character]) {
+    return latinToMorse[character]
+} else {
+    return ''; 
+}
+
+
+}
+const resultMorse = translateLatinCharacter('A'); 
+console.log(resultMorse)
+
+
+function enCode (text) {
+    const characterList = getLatinCharacterList(text)
+let morseMessage = ''
+
+for (let i = 0; i < characterList.length; i++) {
+const morseCharacter = translateLatinCharacter(characterList[i])
+
+if (morseCharacter !== '') {
+    morseMessage += morseCharacter
+}
+
+if(i < characterList.length - 1) {
+    morseMessage += ' ';
+}
+}
+
+return morseMessage
+}
+
+const encodedMessage = enCode("Hello, World")
+console.log(encodedMessage)
